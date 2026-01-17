c6983c1a88f1db4c1f2bc920b75a9f56d8f18996
vboxuser <xzavier.aguilar.08@gmail.com>
2026-01-17 18:46:06 +0000

    alomost there, just gotta get the 107 line to work, REPORT_ID_KEYBOARD is not working

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "pico/stdlib.h"
 #include "tusb.h"
+//#include "tusb_hid.h"
 #include "device/usbd.h"
 #include "class/hid/hid_device.h"
 #include <stdio.h>
@@ -16,7 +17,17 @@
 
 const uint faceButtons[]={0,1,2,3,4,5,6,7};
 //if the orgional keys wante to be used, mostly u,j,i,k,o,l,p, and ; (yes, the semi-colin key)
-
+const uint8_t faceKeys[]=
+{
+    HID_KEY_U,
+    HID_KEY_J,
+    HID_KEY_I,
+    HID_KEY_K,
+    HID_KEY_O,
+    HID_KEY_L,
+    HID_KEY_P,
+    HID_KEY_SEMICOLON
+};
 
 // #define up 8
 // #define down 9
@@ -24,6 +35,13 @@ const uint faceButtons[]={0,1,2,3,4,5,6,7};
 // #define right 11
 
 const uint directions[]={8,9,10,11};
+const uint8_t directionKeys[]=
+{
+    HID_KEY_W,
+    HID_KEY_A,
+    HID_KEY_S,
+    HID_KEY_D
+};
 //basic up down left and right 
 
 // #define extraButton1 12
@@ -33,7 +51,19 @@ const uint directions[]={8,9,10,11};
 // #define extraButton5 16
 
 const uint extraButtons[]={12,13,14,15,16};
-//should be stuff like enter, tab, escape, backspace and maybe shift 
+const uint8_t extraKeys[]=
+{
+    HID_KEY_TAB,
+    HID_KEY_ENTER,
+    HID_KEY_ESCAPE,
+    HID_KEY_BACKSPACE,
+    HID_KEY_SPACE
+};
+//should be stuff like enter, tab, escape, backspace and maybe space 
+
+
+
+
 
 int main()
 {
@@ -67,4 +97,14 @@ void set_buttons()
         gpio_init(extraButtons[i]);
         gpio_set_dir(extraButtons[i], GPIO_IN);
     }
+}
+
+void key_sendy(uint8_t keyP)
+{
+    if (tud_hid_ready())
+    {
+        uint8_t keyReader[]={keyP,0,0,0,0,0};
+//        tud_hid_keyboard_report(, 0, keyReader);
+    }
+
 }
